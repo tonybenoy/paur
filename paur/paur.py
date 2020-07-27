91e4c47e6fa4ba6e5f7a2932a99f8196718ae708
Tony Benoy <me@tonybenoy.com>
2020-07-27 15:01:13 +0530

    find pkg

--- paur/paur.py
+++ paur/paur.py
@@ -1,7 +1,7 @@
 from typing import Optional
 import typer
 from init.utils import create_default_config
-
+from find import find
 app = typer.Typer()
 
 
@@ -10,6 +10,8 @@ def init(sub_command: Optional[str] = typer.Argument(None)) -> None:
     clean = True if sub_command else False
     if create_default_config(clean=clean):
         typer.echo(f"Config successfully created!")
+
+app.add_typer(find.app, name="find")
 
 
 if __name__ == "__main__":
